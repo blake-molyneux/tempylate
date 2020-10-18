b1422f8be44c8abd6b7efd6ae2b6695cf80e52b4
= <=>
2020-10-18 16:47:02 +1100

    Added caching functions to the pypi request

--- tempylate/namepkg/namepkg.py
+++ tempylate/namepkg/namepkg.py
@@ -18,6 +18,9 @@ __license__ = "MIT"
 __version__ = "0.0.1.dev0"
 
 
+import os
+import json
+import pathlib
 import urllib.request
 import urllib.parse
 
@@ -31,23 +34,38 @@ class Name:
         A topic/theme subject area. Used to generate a new name.
     name : str, Optional
         An explicit name.
-    
+    save : bool, Optional
+        If set to true, any json data recieved will be saved. False by
+        default.
+    out : pathlib.Path, Optional
+        Specifies the dir to save the output json. Defaults to __file__.
+
     Attributes
     ----------
-    theme : str, Optional
-        A topic/theme subject area. Used to generate a new name.
-    name : str, Optional
-        An explicit name.
+    theme : str
+        A topic/theme subject area.
+    name : str
+        The name selected by the user, generated or specified.
     available : bool
         Whether or not the name is available on PyPI.
-    
-    """
-    def __init__(self,
-                 theme : str = '',
-                 name : str = ''):
+    save : bool
+        If set to true, any json data recieved will be saved. False by
+        default.
+    out : pathlib.Path
+        Specifies the dir to save the output json. Defaults to __file__.
 
+    """
+    def __init__(self, 
+                 theme : str = '', 
+                 name : str = '',
+                 save : bool = False, 
+                 out : pathlib.Path() = (pathlib.Path(
+                       os.path.dirname(os.path.realpath(__file__)))
+                       / pathlib.Path('data/cache.json'))):
         self.name = name
         self.theme = theme
+        self.save = save
+        self.out = out
         self.available = False
 
     def gen_new_name(self):
@@ -64,10 +82,20 @@ class Name:
         
         request = urllib.request.Request(url)
         try:
-            urllib.request.urlopen(request)
-            available = False
+            # Attempt to get a packages json data from pypi
+            with urllib.request.urlopen(request) as f:
+                dict_in = json.loads(f.read().decode("utf-8"))
+            
+            if (self.save):
+                # Output the result to a local file
+                output_path = self.out
+                with open(output_path, 'a') as f:
+                    f.write(json.dumps(dict_in, sort_keys=True, indent=4))
+
+            available = False # Package is used by someone
+        
         except urllib.error.HTTPError as e:
-            available = True
+            available = True  # Package is available
 
         self.available = available
         pass
@@ -75,9 +103,12 @@ class Name:
     def mk_url(self):
         """Construct the PyPI URL for the chosen name."""
         name = self.name
-        base = 'https://pypi.org/project/'
-        
-        self.url = urllib.parse.urljoin(base, name)
+        url_base = 'https://pypi.org/pypi/'
+
+        url_suffix = f'{name}/json'
+        url = urllib.parse.urljoin(url_base, url_suffix)
+
+        self.url = url
         pass
 
 def main():
@@ -91,6 +122,7 @@ def main():
         print('\n#=================#')
         input_name = input('Input project name:\n')
         name = Name(name=input_name)
+        name.save = True # Save the output
         name.check()
         
         if (name.available):
